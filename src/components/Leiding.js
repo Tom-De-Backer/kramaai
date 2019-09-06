32afd68aaea0251e6bd232a4439db6a21c4a80f3
Tom De Backer <tom_debacker@msn.com>
2019-09-06 12:05:38 +0200

    Leiding pagina

--- src/components/Leiding.js
+++ src/components/Leiding.js
@@ -1,11 +1,22 @@
 import React, { Component } from "react";
 
+import "../styles/Leiding.css";
+import LeidingTable from "./LeidingTable";
+
 class Leiding extends Component {
   render() {
     return (
-      <div>
-        <h1>Leiding</h1>
-      </div>
+      <section className="infoLeiding">
+        <div className="title">Leiding 2019-2020</div>
+        <p>
+          De vetgedrukte namen zijn de <b>takverantwoordelijken</b>, aarzel niet
+          om hen te contacteren. Je kan ook altijd een mailtje sturen naar de
+          hele takleiding door naar tak@kramaai.be te mailen (bv.
+          welpen@kramaai.be om de welpenleiding te contacteren).
+        </p>
+        <br />
+        <LeidingTable />
+      </section>
     );
   }
 }
---
+++ src/components/LeidingTable.js
@@ -0,0 +1,206 @@
+import React from "react";
+
+import "../styles/Leiding.css";
+
+const LeidingTable = () => {
+  return (
+    <div>
+      <table className="table">
+        <tr>
+          <th></th>
+          <th>Kapoenen</th>
+          <th></th>
+        </tr>
+
+        <tr>
+          <td>
+            <b>Lise Michiels</b>
+          </td>
+          <td>
+            <b>Opmerkelijke mus</b>
+          </td>
+          <td>
+            <b>lise@kramaai.be</b>
+          </td>
+        </tr>
+        <tr>
+          <td>Lise Michiels</td>
+          <td>Opmerkelijke mus</td>
+          <td>lise@kramaai.be</td>
+        </tr>
+        <tr>
+          <td>Lise Michiels</td>
+          <td>Opmerkelijke mus</td>
+          <td>lise@kramaai.be</td>
+        </tr>
+        <tr>
+          <td>Lise Michiels</td>
+          <td>Opmerkelijke mus</td>
+          <td>lise@kramaai.be</td>
+        </tr>
+        <tr>
+          <td>Lise Michiels</td>
+          <td>Opmerkelijke mus</td>
+          <td>lise@kramaai.be</td>
+        </tr>
+      </table>
+      <br />
+      <br />
+      <table className="table">
+        <tr>
+          <th></th>
+          <th>Welpen</th>
+          <th></th>
+        </tr>
+        <tr>
+          <td>
+            <b>Lennart Beeckman</b>
+          </td>
+          <td>
+            <b>Toegewijde Steltral</b>
+          </td>
+          <td>
+            <b>lennart@kramaai.be</b>
+          </td>
+        </tr>
+        <tr>
+          <td>Sander Bettens</td>
+          <td>Standvastige Bergduivel</td>
+          <td>sander@kramaai.be</td>
+        </tr>
+        <tr>
+          <td>Lennart Beeckman</td>
+          <td>Toegewijde Steltral</td>
+          <td>lennart@kramaai.be</td>
+        </tr>
+        <tr>
+          <td>Lennart Beeckman</td>
+          <td>Toegewijde Steltral</td>
+          <td>lennart@kramaai.be</td>
+        </tr>
+      </table>
+      <br />
+      <br />
+      <table className="table">
+        <tr>
+          <th></th>
+          <th>Bevers</th>
+          <th></th>
+        </tr>
+        <tr>
+          <td>
+            <b>Lennart Beeckman</b>
+          </td>
+          <td>
+            <b>Toegewijde Steltral</b>
+          </td>
+          <td>
+            <b>lennart@kramaai.be</b>
+          </td>
+        </tr>
+        <tr>
+          <td>Sander Bettens</td>
+          <td>Standvastige Bergduivel</td>
+          <td>sander@kramaai.be</td>
+        </tr>
+        <tr>
+          <td>Lennart Beeckman</td>
+          <td>Toegewijde Steltral</td>
+          <td>lennart@kramaai.be</td>
+        </tr>
+        <tr>
+          <td>Lennart Beeckman</td>
+          <td>Toegewijde Steltral</td>
+          <td>lennart@kramaai.be</td>
+        </tr>
+      </table>
+      <br />
+      <br />
+      <table className="table">
+        <tr>
+          <th></th>
+          <th>Jong-Givers</th>
+          <th></th>
+        </tr>
+        <tr>
+          <td>
+            <b>Lennart Beeckman</b>
+          </td>
+          <td>
+            <b>Toegewijde Steltral</b>
+          </td>
+          <td>
+            <b>lennart@kramaai.be</b>
+          </td>
+        </tr>
+        <tr>
+          <td>Sander Bettens</td>
+          <td>Standvastige Bergduivel</td>
+          <td>sander@kramaai.be</td>
+        </tr>
+        <tr>
+          <td>Lennart Beeckman</td>
+          <td>Toegewijde Steltral</td>
+          <td>lennart@kramaai.be</td>
+        </tr>
+      </table>
+      <br />
+      <br />
+      <table className="table">
+        <tr>
+          <th></th>
+          <th>Givers</th>
+          <th></th>
+        </tr>
+        <tr>
+          <td>
+            <b>Lennart Beeckman</b>
+          </td>
+          <td>
+            <b>Toegewijde Steltral</b>
+          </td>
+          <td>
+            <b>lennart@kramaai.be</b>
+          </td>
+        </tr>
+        <tr>
+          <td>Sander Bettens</td>
+          <td>Standvastige Bergduivel</td>
+          <td>sander@kramaai.be</td>
+        </tr>
+        <tr>
+          <td>Lennart Beeckman</td>
+          <td>Toegewijde Steltral</td>
+          <td>lennart@kramaai.be</td>
+        </tr>
+      </table>
+      <br />
+      <br />
+      <table className="table">
+        <tr>
+          <th></th>
+          <th>Jins</th>
+          <th></th>
+        </tr>
+        <tr>
+          <td>
+            <b>Lennart Beeckman</b>
+          </td>
+          <td>
+            <b>Toegewijde Steltral</b>
+          </td>
+          <td>
+            <b>lennart@kramaai.be</b>
+          </td>
+        </tr>
+        <tr>
+          <td>Sander Bettens</td>
+          <td>Standvastige Bergduivel</td>
+          <td>sander@kramaai.be</td>
+        </tr>
+      </table>
+    </div>
+  );
+};
+
+export default LeidingTable;
