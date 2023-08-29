81989f47f36d1bd314466580ac049dcf6c9fc3c6
Merlijn <m>
2023-08-29 09:03:43 +0200

    chore(home): Add some calendar entries

--- src/components/Home.js
+++ src/components/Home.js
@@ -12,15 +12,15 @@ import SideSpace from "./SideSpace";
 import WidgetSlideshow from "./slideShow/WidgetSlideshow";
 
 class Home extends Component {
-  render() {
-    ReactGA.pageview(window.location.pathname + window.location.search);
-    return (
-      <div className="home">
-        <SlideEffect className="slideEffect" />
-        <section className="info">
-          <SideSpace />
-          <div className="news">
-            {/* <NewsItem
+    render() {
+        ReactGA.pageview(window.location.pathname + window.location.search);
+        return (
+            <div className="home">
+                <SlideEffect className="slideEffect" />
+                <section className="info">
+                    <SideSpace />
+                    <div className="news">
+                        {/* <NewsItem
               title="Vakesweekend 2020"
               text='<p 
                       style=" padding-left: 10px;
@@ -41,7 +41,7 @@ class Home extends Component {
               imageUrl="https://kramaaiblog.files.wordpress.com/2020/06/vakesweekend.jpg"
             /> */}
 
-            {/* <NewsItem
+                        {/* <NewsItem
               title="Scoutsjaar 2021-2022 "
               text='<p 
               style=" padding-left: 10px;
@@ -54,42 +54,7 @@ class Home extends Component {
               imageUrl="https://kramaaiblog.files.wordpress.com/2021/08/diaavond2021.jpg"
             /> */}
 
-            {/* <NewsItem
-              title="Italiaans etentje 2020 (AFGELAST)"
-              text='<p 
-              style=" padding-left: 10px;
-                      padding-right: 20px;
-                      padding-bottom: 20px;
-                      padding-top: 20px;
-                      font-size: 1.17em;
-                      ">
-                      Beste ouders en leden, 
-                      <br /><br />
-                      We hebben besloten om ons Italiaans etentje te annuleren. Gezien de huidige situatie vinden we het niet meer verantwoord om op dit moment een evenement te organiseren. Maar uitstel is geen afstel. 
-                      Zorg goed voor jezelf en voor elkaar!<br /><br />
-                      Een stevige linkerelleboog<br />
-                      Leiding en kookploeg van Scouts Kramaai Mollem
-                      </p>'
-              imageUrl=" "
-            />
-
-            <NewsItem
-              title="Santé op onze jubilé: 45 jarig bestaan Scouts Mollem (Uitgesteld)"
-              text=" "
-              imageUrl="https://kramaaiblog.files.wordpress.com/2020/01/jubilee40jaarscouts.jpg"
-            />
-            <h2>
-              <center><a href="http://jubile.kramaai.be" target="_blank" rel="noopener noreferrer">Meer info op jubile.kramaai.be</a></center>
-            </h2>
-            <br /> */}
-          {/* new Date(2022, 10, 7, 0, 0, 0, 0) > new Date() && (
-            <NewsItem
-            title="Italiaans etentje"
-              text=" "
-            imageUrl="https://www.kramaai.be/images/italiaans_etentje_2022.jpeg"
-            />
-          )*/}
-		{/* <NewsItem
+                        {/* <NewsItem
 		  title="The White, Little & Grey (K)Night 2023"
 		  text='<p 
 		  style=" padding-left: 10px;
@@ -107,7 +72,7 @@ class Home extends Component {
 		  imageUrl="https://kramaai.be/images/TWK.jpg"
 		  imageLink="https://www.facebook.com/TheWhiteKnight1730"
 		/> */}
-          {/* new Date(2022, 8, 4, 13, 0, 0, 0) < new Date() && (
+                        {/* new Date(2022, 8, 4, 13, 0, 0, 0) < new Date() && (
             <NewsItem
             title="Kramaai"
               text='<p 
@@ -127,7 +92,7 @@ class Home extends Component {
             imageUrl=" "
             />
           )*/}
-          { <NewsItem
+                        {/* <NewsItem
             title="Kampfoto's"
               text="<p 
               style=' padding-left: 10px;
@@ -144,22 +109,50 @@ class Home extends Component {
               <br/>De Leiding
             <p/>"
             imageUrl=" "
-            /> }
-          </div>
+            /> */}
+                    </div>
 
-          <div className="calender">
-            <h2 className="titleCalendar">Kalender</h2>
-            <CalendarItem date="9 september" event="Dia-avond" until={new Date(2023, 8, 10)} />
-			<CalendarItem date="17 september" event="Eerste vergadering" until={new Date(2023, 8, 17, 17)}/>
-            <br />
-            <WidgetSlideshow />
-            <SideWidget />
-          </div>
-          <SideSpace />
-        </section>
-      </div>
-    );
-  }
+                    <div className="calender">
+                        <h2 className="titleCalendar">Kalender</h2>
+                        <CalendarItem
+                            date="9 September"
+                            event="Dia-avond"
+                            until={new Date(2023, 8, 10)}
+                        />
+                        <CalendarItem
+                            date="17 September"
+                            event="Eerste vergadering"
+                            until={new Date(2023, 8, 17, 17)}
+                        />
+                        <CalendarItem
+                            date="4 & 5 November"
+                            event="Italiaans etentje"
+                            until={new Date(2023, 10, 6)}
+                        />
+                        <CalendarItem
+                            date="24 December"
+                            event="Jeneveravond"
+                            until={new Date(2023, 11, 24)}
+                        />
+                        <CalendarItem
+                            date="5 & 6 April"
+                            event="White, Little & Grey (K)night"
+                            until={new Date(2024, 3, 7)}
+                        />
+                        <CalendarItem
+                            date="3-13 Augustus"
+                            event="Kamp"
+                            until={new Date(2024, 7, 14)}
+                        />
+                        <br />
+                        <WidgetSlideshow />
+                        <SideWidget />
+                    </div>
+                    <SideSpace />
+                </section>
+            </div>
+        );
+    }
 }
 
 export default Home;
