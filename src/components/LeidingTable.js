5c7c44b1b67a0765f8c3174c8052338b96a0a05c
Merlijn <m>
2023-08-30 19:31:26 +0200

    feat(leiding): Move leiding into separate objects

--- src/components/LeidingTable.js
+++ src/components/LeidingTable.js
@@ -1,314 +1,137 @@
 import React from "react";
 
 import "../styles/Leiding.css";
+import leiding from "./Leiding_object.js";
 
 const LeidingTable = () => {
-  return (
-    <div>
-      {/* new Date(2020, 8, 5, 22, 0, 0, 0) = Sat Sep 05 2020 22:00:00 GMT+0200 (Central European Summer Time) */}
-      {new Date(2021, 8, 6, 22, 0, 0, 0) < new Date() && (
+    function getLeiding(leidingArray, number = false) {
+        return (
+            <tbody>
+                {leidingArray.map((leiding, index) => {
+                    let first = index == 0 && !number;
+                    return (
+                        <tr>
+                            <td>
+                                {(first && <b>{leiding.name}</b>) ||
+                                    leiding.name}
+                            </td>
+                            <td>
+                                {(first && <b>{leiding.totem}</b>) ||
+                                    leiding.totem}
+                            </td>
+                            <td>
+                                {(first && <b>{leiding.email}</b>) ||
+                                    leiding.email}
+                            </td>
+                            {number && <td>{leiding.number}</td>}
+                        </tr>
+                    );
+                })}
+            </tbody>
+        );
+    }
+
+    return (
         <div>
-          <br />
-          <div className="titleTable" id="kapoenen">
-            <div>Kapoenen</div>
-            <a className="emailTable" href="mailto:kapoenen@kramaai.be">
-              Kapoenen@kramaai.be
-            </a>
-          </div>
-          <table className="table">
-            <tbody>
-              <tr>
-                <td>
-                  <b>Hanne Esselens</b>
-                </td>
-                <td>
-                  <b>Ijverige Otter</b>
-                </td>
-                <td>
-                  <b>Hanne@kramaai.be</b>
-                </td>
-              </tr>
-              <tr>
-                <td>Flore Esselens</td>
-                <td>Goedlachse Springbok</td>
-                <td>Flore@kramaai.be</td>
-              </tr>
-              <tr>
-                <td>Catherine Heyvaert</td>
-                <td>Goedlachs Konijn</td>
-                <td>Catherine@kramaai.be</td>
-              </tr>
-              <tr>
-                <td>Andreas Verhoeven</td>
-                <td>Oprechte Newfoundlander</td>
-                <td>Andreas@kramaai.be</td>
-              </tr>
-              <tr>
-                <td>Merlijn Devroede</td>
-                <td>Eigenaardig Penseelaapje</td>
-                <td>Merlijn@kramaai.be</td>
-              </tr>
-              <tr>
-                <td>Senne Lauwers</td>
-                <td>Schalkse Brulaap</td>
-                <td>Senne@kramaai.be</td>
-              </tr>
-              <tr>
-                <td>Roxanne Hoornaert</td>
-                <td>Unieke Fazant</td>
-                <td>Roxanne@kramaai.be</td>
-              </tr>
-            </tbody>
-          </table>
-          <br />
-          <br />
-          <div className="titleTable" id="welpen">
-            <div>Welpen</div>
-            <a className="emailTable" href="mailto:welpen@kramaai.be">
-              Welpen@kramaai.be
-            </a>
-          </div>
-          <table className="table">
-            <tbody>
-              <tr>
-                <td>
-                  <b>Lennart Beeckman</b>
-                </td>
-                <td>
-                  <b>Toegewijde Steltral</b>
-                </td>
-                <td>
-                  <b>Lennart@kramaai.be</b>
-                </td>
-              </tr>
-              <tr>
-                <td>Delphine Ilsbroux</td>
-                <td>Uitbundige Bezoar</td>
-                <td>Delphine@kramaai.be</td>
-              </tr>
-              <tr>
-                <td>Tijs De Jonge</td>
-                <td></td>
-                <td>Tijs@kramaai.be</td>
-              </tr>
-              <tr>
-                <td> Frederic Van Praet </td>
-                <td> Genereuze Haan </td>
-                <td> Frederic@kramaai.be </td>
-              </tr>
-              <tr>
-                <td>Jari Heyvaert</td>
-                <td></td>
-                <td>Jari@kramaai.be</td>
-              </tr>
-              <tr>
-                <td>Kobe Gysens</td>
-                <td>Ondeugende Ekster</td>
-                <td>Kobe@kramaai.be</td>
-              </tr>
-            </tbody>
-          </table>
-          <br />
-          <br />
-          <div className="titleTable" id="bevers">
-            <div>Bevers</div>
-            <a className="emailTable" href="mailto:bevers@kramaai.be">
-              Bevers@kramaai.be
-            </a>
-          </div>
-          <table className="table">
-            <tbody>
-              <tr>
-                <td>
-                  <b>Fien De Jonge</b>
-                </td>
-                <td>
-                  <b>Vlotte Wasbeer</b>
-                </td>
-                <td>
-                  <b>FienDeJonge@kramaai.be</b>
-                </td>
-              </tr>
-              <tr>
-                <td>Jules Eeman</td>
-                <td>Nonchalante Lepelaar</td>
-                <td>Jules@kramaai.be</td>
-              </tr>
-              <tr>
-                <td>Jasper Beeckman</td>
-                <td>Sympathieke Spitsvogel</td>
-                <td>Jasper@kramaai.be</td>
-              </tr>
-              <tr>
-                <td>Ferre Luppens</td>
-                <td>Ingetogen Kameel</td>
-                <td>Ferre@kramaai.be</td>
-              </tr>
-              <tr>
-                <td>Bram Esselens</td>
-                <td>Avontuurlijke Stokstaart</td>
-                <td>Bram@kramaai.be</td>
-              </tr>
-              <tr>
-                <td>Romain Wouters</td>
-                <td>Respectvolle Schaarbek</td>
-                <td>Romain@kramaai.be</td>
-              </tr>
-            </tbody>
-          </table>
-          <br />
-          <br />
-          <div className="titleTable" id="jonggivers">
-            <div>Jong-Givers</div>
-            <a className="emailTable" href="mailto:jonggivers@kramaai.be">
-              Jonggivers@kramaai.be
-            </a>
-          </div>
-          <table className="table">
-            <tbody>
-              <tr>
-                <td>
-                  <b>Matisse Eeman</b>
-                </td>
-                <td>
-                  <b>Dromerige Spreeuw</b>
-                </td>
-                <td>
-                  <b>Matisse@kramaai.be</b>
-                </td>
-              </tr>
-              <tr>
-                <td>Manse Bouckaert</td>
-                <td>Onstuimige Streepmuis</td>
-                <td>Manse@kramaai.be</td>
-              </tr>
-              <tr>
-                <td> Inne Van Lil</td>
-                <td> Enthousiaste Toekan</td>
-                <td> Inne@kramaai.be</td>
-              </tr>
-              <tr>
-                <td> Gilles Van Den Cruyce</td>
-                <td> Artistieke Sifaka</td>
-                <td> Gilles@kramaai.be</td>
-              </tr>
-              <tr>
-                <td>Kobe Bogaert</td>
-                <td>Stoutmoedige Walrus</td>
-                <td>KobeBogaert@kramaai.be</td>
-              </tr>
-              <tr>
-                <td>David De Knop</td>
-                <td>Ijverige Alpaca</td>
-                <td>David@kramaai.be</td>
-              </tr>
-            </tbody>
-          </table>
-          <br />
-          <br />
-          <div className="titleTable" id="givers">
-            <div>Givers</div>
-            <a className="emailTable" href="mailto:givers@kramaai.be">
-              Givers@kramaai.be
-            </a>
-          </div>
-          <table className="table">
-            <tbody>
-              <tr>
-                <td>
-                  <b>Lorraine Wouters</b>
-                </td>
-                <td>
-                  <b>Goedhartig Sikahert</b>
-                </td>
-                <td>
-                  <b>Lorraine@kramaai.be</b>
-                </td>
-              </tr>
-              <tr>
-                <td> Daan Capitaine </td>
-                <td> Fiere Jan-van-Gent </td>
-                <td> Daan@kramaai.be </td>
-              </tr>
-              <tr>
-                <td> Mats Verdoodt </td>
-                <td> Verrassende Jabiroe </td>
-                <td> Mats@kramaai.be </td>
-              </tr>
-              <tr>
-                <td>Arthur Van der Smissen</td>
-                <td>Onverschrokken Lori</td>
-                <td>Arthur@kramaai.be</td>
-              </tr>
-            </tbody>
-          </table>
-          <br />
-          <br />
-          <div className="titleTable" id="jins">
-            <div>Jins</div>
-            <a className="emailTable" href="mailto:jins@kramaai.be">
-              Jins@kramaai.be
-            </a>
-          </div>
-          <table className="table">
-            <tbody>
-              <tr>
-                <td>
-                  <b>Liesa Heyvaert</b>
-                </td>
-                <td>
-                  <b>Jolige Tijger</b>
-                </td>
-                <td>
-                  <b>Liesa@kramaai.be</b>
-                </td>
-              </tr>
-              <tr>
-                <td> Karo Van Lil </td>
-                <td> Begane Katoenstaart </td>
-                <td> Karo@kramaai.be </td>
-              </tr>
-              <tr>
-                <td>Tom De Backer</td>
-                <td>Gezellige Wasbeer</td>
-                <td>Tom@kramaai.be</td>
-              </tr>
-            </tbody>
-          </table>
-          <br />
-          <br />
-          <div className="titleTable" id="groupsleiding">
-            <div>Groepsleiding</div>
-            <a className="emailTable" href="mailto:groepsleiding@kramaai.be">
-              Groepsleiding@kramaai.be
-            </a>
-          </div>
-          <table className="table">
-            <tbody>
-              <tr>
-                <td>Bram Esselens</td>
-                <td>Avontuurlijke Stokstaart</td>
-                <td>Bram@kramaai.be</td>
-                <td>+32 468 15 25 19</td>
-              </tr>
-              <tr>
-                <td>Kobe Gysens</td>
-                <td>Ondeugende Ekster</td>
-                <td>Kobe@kramaai.be</td>
-                <td>+32 474 67 70 75</td>
-              </tr>
-              <tr>
-                <td>Jasper Beeckman</td>
-                <td>Sympathieke Spitsvogel</td>
-                <td>Jasper@kramaai.be</td>
-                <td>+32 468 29 27 35</td>
-              </tr>
-            </tbody>
-          </table>
+            {/* new Date(2020, 8, 5, 22, 0, 0, 0) = Sat Sep 05 2020 22:00:00 GMT+0200 (Central European Summer Time) */}
+            {new Date(2021, 8, 6, 22, 0, 0, 0) < new Date() && (
+                <div>
+                    <br />
+                    <div className="titleTable" id="kapoenen">
+                        <div>Kapoenen</div>
+                        <a
+                            className="emailTable"
+                            href="mailto:kapoenen@kramaai.be"
+                        >
+                            Kapoenen@kramaai.be
+                        </a>
+                    </div>
+                    <table className="table">
+                        {getLeiding(leiding.kapoenen)}
+                    </table>
+                    <br />
+                    <br />
+                    <div className="titleTable" id="welpen">
+                        <div>Welpen</div>
+                        <a
+                            className="emailTable"
+                            href="mailto:welpen@kramaai.be"
+                        >
+                            Welpen@kramaai.be
+                        </a>
+                    </div>
+                    <table className="table">
+                        {getLeiding(leiding.welpen)}
+                    </table>
+                    <br />
+                    <br />
+                    <div className="titleTable" id="bevers">
+                        <div>Bevers</div>
+                        <a
+                            className="emailTable"
+                            href="mailto:bevers@kramaai.be"
+                        >
+                            Bevers@kramaai.be
+                        </a>
+                    </div>
+                    <table className="table">
+                        {getLeiding(leiding.bevers)}
+                    </table>
+                    <br />
+                    <br />
+                    <div className="titleTable" id="jonggivers">
+                        <div>Jong-Givers</div>
+                        <a
+                            className="emailTable"
+                            href="mailto:jonggivers@kramaai.be"
+                        >
+                            Jonggivers@kramaai.be
+                        </a>
+                    </div>
+                    <table className="table">
+                        {getLeiding(leiding.jonggivers)}
+                    </table>
+                    <br />
+                    <br />
+                    <div className="titleTable" id="givers">
+                        <div>Givers</div>
+                        <a
+                            className="emailTable"
+                            href="mailto:givers@kramaai.be"
+                        >
+                            Givers@kramaai.be
+                        </a>
+                    </div>
+                    <table className="table">
+                        {getLeiding(leiding.givers)}
+                    </table>
+                    <br />
+                    <br />
+                    <div className="titleTable" id="jins">
+                        <div>Jins</div>
+                        <a className="emailTable" href="mailto:jins@kramaai.be">
+                            Jins@kramaai.be
+                        </a>
+                    </div>
+                    <table className="table">{getLeiding(leiding.jins)}</table>
+                    <br />
+                    <br />
+                    <div className="titleTable" id="groupsleiding">
+                        <div>Groepsleiding</div>
+                        <a
+                            className="emailTable"
+                            href="mailto:groepsleiding@kramaai.be"
+                        >
+                            Groepsleiding@kramaai.be
+                        </a>
+                    </div>
+                    <table className="table">
+                        {getLeiding(leiding.groep, true)}
+                    </table>
+                </div>
+            )}
         </div>
-      )}
-    </div>
-  );
+    );
 };
 
 export default LeidingTable;
---
+++ src/components/Leiding_object.js
@@ -0,0 +1,169 @@
+let leiding = {
+    Flore: {
+        name: "Flore Esselens",
+        totem: "Goedlachse Springbok",
+        email: "Flore@kramaai.be",
+    },
+    Catherine: {
+        name: "Catherine Heyvaert",
+        totem: "Goedlachs Konijn",
+        email: "Catherine@kramaai.be",
+    },
+    Andreas: {
+        name: "Andreas Verhoeven",
+        totem: "Oprechte Newfoundlander",
+        email: "Andreas@kramaai.be",
+    },
+    Merlijn: {
+        name: "Merlijn Devroede",
+        totem: "Eigenaardig Penseelaapje",
+        email: "Merlijn@kramaai.be",
+    },
+    Senne: {
+        name: "Senne Lauwers",
+        totem: "Schalkse Brulaap",
+        email: "Senne@kramaai.be",
+    },
+    Roxanne: {
+        name: "Roxanne Hoornaert",
+        totem: "Unieke Fazant",
+        email: "Roxanne@kramaai.be",
+    },
+    Delphine: {
+        name: "Delphine Ilsbroux",
+        totem: "Uitbundige Bezoar",
+        email: "Delphine@kramaai.be",
+    },
+    Tijs: {
+        name: "Tijs De Jonge",
+        totem: "",
+        email: "Tijs@kramaai.be",
+    },
+    Frederic: {
+        name: "Frederic Van Praet",
+        totem: "Genereuze Haan",
+        email: "Frederic@kramaai.be",
+    },
+    Jari: {
+        name: "Jari Heyvaert",
+        totem: "",
+        email: "Jari@kramaai.be",
+    },
+    KobeG: {
+        name: "Kobe Gysens",
+        totem: "Ondeugende Ekster",
+        email: "Kobe@kramaai.be",
+        number: "+32 474 67 70 75",
+    },
+    FienDJ: {
+        name: "Fien De Jonge",
+        totem: "Vlotte Wasbeer",
+        email: "FienDeJonge@kramaai.be",
+    },
+    Jules: {
+        name: "Jules Eeman",
+        totem: "Nonchalante Lepelaar",
+        email: "Jules@kramaai.be",
+    },
+    Jasper: {
+        name: "Jasper Beeckman",
+        totem: "Sympathieke Spitsvogel",
+        email: "Jasper@kramaai.be",
+        number: "+32 468 29 27 35",
+    },
+    Ferre: {
+        name: "Ferre Luppens",
+        totem: "Ingetogen Kameel",
+        email: "Ferre@kramaai.be",
+    },
+    Romain: {
+        name: "Romain Wouters",
+        totem: "Respectvolle Schaarbek",
+        email: "Romain@kramaai.be",
+    },
+    Matisse: {
+        name: "Matisse Eeman",
+        totem: "Dromerige Spreeuw",
+        email: "Matisse@kramaai.be",
+    },
+    Manse: {
+        name: "Manse Bouckaert",
+        totem: "Onstuimige Streepmuis",
+        email: "Manse@kramaai.be",
+    },
+    Inne: {
+        name: "Inne Van Lil",
+        totem: "Enthousiaste Toekan",
+        email: "Inne@kramaai.be",
+    },
+    Gilles: {
+        name: "Gilles Van Den Cruyce",
+        totem: "Artistieke Sifaka",
+        email: "Gilles@kramaai.be",
+    },
+    KobeB: {
+        name: "Kobe Bogaert",
+        totem: "Stoutmoedige Walrus",
+        email: "KobeBogaert@kramaai.be",
+    },
+    David: {
+        name: "David De Knop",
+        totem: "Ijverige Alpaca",
+        email: "David@kramaai.be",
+    },
+    Lorraine: {
+        name: "Lorraine Wouters",
+        totem: "Goedhartig Sikahert",
+        email: "Lorraine@kramaai.be",
+    },
+    Mats: {
+        name: "Mats Verdoodt ",
+        totem: "Verrassende Jabiroe ",
+        email: "Mats@kramaai.be ",
+    },
+    Arthur: {
+        name: "Arthur Van der Smissen",
+        totem: "Onverschrokken Lori",
+        email: "Arthur@kramaai.be",
+    },
+    Lise: {
+        name: "Lise Van Geertruyen",
+        totem: "",
+        email: "Lise@kramaai.be",
+    },
+    Wout: { name: "Wout Huysman", totem: "", email: "Wout@kramaai.be" },
+    Robbe: { name: "Robbe Tops", totem: "", email: "Robbe@kramaai.be" },
+    Joren: { name: "Joren Tops", totem: "", email: "Joren@kramaai.be" },
+    VictorC: {
+        name: "Victor Carle",
+        totem: "",
+        email: "Victor.carle@kramaai.be",
+    },
+    VictorB: {
+        name: "Victor Boterdaele",
+        totem: "",
+        email: "Victor.boterdaele@kramaa.be",
+    },
+    Remco: { name: "Remco Looverie", totem: "", email: "Remco@kramaai.be" },
+    Amber: { name: "Amber Gysens", totem: "", email: "Amber@kramaai.be" },
+    Tibo: { name: "Tibo De Bondt", totem: "", email: "Tibo@kramaai.be" },
+    Toon: { name: "Toon Esselens", totem: "", email: "Tibo@kramaai.be" },
+    Wiebe: { name: "Wiebe Luppens", totem: "", email: "Wiebe@kramaai.be" },
+};
+
+export default {
+    kapoenen: [
+        leiding.Flore,
+        leiding.Catherine,
+        leiding.Senne,
+        leiding.Andreas,
+        leiding.Roxanne,
+        leiding.Merlijn,
+    ],
+    welpen: [leiding.Inne, leiding.KobeG],
+    bevers: [leiding.Jasper, leiding.Romain],
+    jonggivers: [leiding.Matisse, leiding.Manse],
+    givers: [leiding.Arthur, leiding.Lorraine],
+    jins: [leiding.Toon, leiding.VictorC],
+    groep: [leiding.Jasper, leiding.KobeG],
+};
