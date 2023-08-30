1000316d6d4774113fa25d3273a6b0a3cc115e3b
Merlijn <m>
2023-08-30 18:19:49 +0200

    feat(home): Use JSON file on server for news and calendar

--- src/components/Home.js
+++ src/components/Home.js
@@ -11,7 +11,43 @@ import SideWidget from "./SideWidget";
 import SideSpace from "./SideSpace";
 import WidgetSlideshow from "./slideShow/WidgetSlideshow";
 
+let calendarPromise = new Promise((resolve) => {
+    fetch("https://kramaai.be/public/calendar.json").then((calendar) =>
+        resolve(calendar.json())
+    );
+});
+let newsPromise = new Promise((resolve) => {
+    fetch("https://kramaai.be/public/news.json").then((news) =>
+        resolve(news.json())
+    );
+});
+
 class Home extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            calendarItems: undefined,
+            newsItems: undefined,
+        };
+        this.getCalendarItems = this.getCalendarItems.bind(this);
+        this.getCalendarItems();
+        this.getNewsItems = this.getNewsItems.bind(this);
+        this.getNewsItems();
+    }
+
+    getCalendarItems() {
+        calendarPromise.then((items) => {
+            this.setState((state) => (state.calendarItems = items));
+        });
+    }
+
+    getNewsItems() {
+        newsPromise.then((items) => {
+            this.setState((state) => (state.newsItems = items));
+        });
+    }
+
     render() {
         ReactGA.pageview(window.location.pathname + window.location.search);
         return (
@@ -20,130 +56,42 @@ class Home extends Component {
                 <section className="info">
                     <SideSpace />
                     <div className="news">
-                        {/* <NewsItem
-              title="Vakesweekend 2020"
-              text='<p 
-                      style=" padding-left: 10px;
-                              padding-right: 20px;
-                              padding-bottom: 20px;
-                              padding-top: 20px;
-                              font-size: 1.17em;
-                              ">
-                              
-                    Dag papa&rsquo;s en mannelijke vrijwilligers,
-                    <br /><br />
-                    Hopelijk staat het weekend van 16-18 oktober al aangeduid in jullie agenda want dan gaan wij op vakesweekend! 
-                    We weten dat het in deze tijden geen garantie is dat dit weekend zal mogen doorgaan maar we hopen op voldoende versoepelde maatregelen. 
-                    Indien we dit alsnog moeten uitstellen worden jullie volledig terugbetaald.
-                    <br /><br />
-                    Voor je offici&euml;le inschrijving en voor meer info: <a href="mailto:vakesweekend@kramaai.be">vakesweekend@kramaai.be</a>
-                    </p>'
-              imageUrl="https://kramaaiblog.files.wordpress.com/2020/06/vakesweekend.jpg"
-            /> */}
-
-                        {/* <NewsItem
-              title="Scoutsjaar 2021-2022 "
-              text='<p 
-              style=" padding-left: 10px;
-                      padding-right: 20px;
-                      padding-bottom: 20px;
-                      padding-top: 20px;
-                      font-size: 1.17em;
-                      ">
-                      </p>'
-              imageUrl="https://kramaaiblog.files.wordpress.com/2021/08/diaavond2021.jpg"
-            /> */}
-
-                        {/* <NewsItem
-		  title="The White, Little & Grey (K)Night 2023"
-		  text='<p 
-		  style=" padding-left: 10px;
-				  padding-right: 20px;
-				  padding-bottom: 20px;
-				  padding-top: 20px;
-				  font-size: 1.17em;
-				  ">
-				  Vrijdag 7 april => <a href="https://www.facebook.com/TheWhiteKnight1730">The White (K)Night</a>
-				  <br />
-				  Zaterdag 8 april => <a href="https://www.facebook.com/events/949279902741165/">The Little (K)Night</a>
-				  <br />
-				  Zaterdag 8 april => <a href="https://www.facebook.com/events/s/the-grey-knight-2023/1263880664485667/">The Grey (K)Night</a>
-				  </p>'
-		  imageUrl="https://kramaai.be/images/TWK.jpg"
-		  imageLink="https://www.facebook.com/TheWhiteKnight1730"
-		/> */}
-                        {/* new Date(2022, 8, 4, 13, 0, 0, 0) < new Date() && (
-            <NewsItem
-            title="Kramaai"
-              text='<p 
-              style=" padding-left: 10px;
-                      padding-right: 20px;
-                      padding-bottom: 20px;
-                      padding-top: 20px;
-                      font-size: 1.17em;
-                      ">
-              Beste ouders en leden,
-              <br/><br/>
-              Vanaf nu staat ook de kramaai van maart, april en mei op onze website. Je kunt deze <a href="https://kramaaiblog.files.wordpress.com/2023/03/kramaai-2223-maart-april-mei.pdf">hier</a> bekijken.
-              <br/>
-              <br/>Een stevige linker
-              <br/>De Leiding
-            <p/>'
-            imageUrl=" "
-            />
-          )*/}
-                        {/* <NewsItem
-            title="Kampfoto's"
-              text="<p 
-              style=' padding-left: 10px;
-                      padding-right: 20px;
-                      padding-bottom: 20px;
-                      padding-top: 20px;
-                      font-size: 1.17em;
-                      '>
-              Beste ouders en leden,
-              <br/><br/>
-              Vanaf nu kunnen jullie ook de foto's van het kamp van 2022 bekijken door <a href='https://photos.app.goo.gl/iPpWPCeNvj8Ew8dH6'>hier</a> te klikken.
-              <br/>
-              <br/>Een stevige linker
-              <br/>De Leiding
-            <p/>"
-            imageUrl=" "
-            /> */}
+                        {(this.state.newsItems &&
+                            ((this.state.newsItems.lenght == 0 && (
+                                <p>
+                                    Er is momenteel geen nieuws, kom later nog
+                                    eens terug.
+                                </p>
+                            )) ||
+                                this.state.newsItems.map((item) => (
+                                    <NewsItem
+                                        key={item.title}
+                                        title={item.title}
+                                        text={item.text}
+                                        imageUrl={item.imageURL}
+                                        imageLink={item.imageLink}
+                                        until={Date.parse(item.until)}
+                                    />
+                                )))) || <p>laden...</p>}
                     </div>
 
                     <div className="calender">
                         <h2 className="titleCalendar">Kalender</h2>
-                        <CalendarItem
-                            date="9 September"
-                            event="Dia-avond"
-                            until={new Date(2023, 8, 10)}
-                        />
-                        <CalendarItem
-                            date="17 September"
-                            event="Eerste vergadering"
-                            until={new Date(2023, 8, 17, 17)}
-                        />
-                        <CalendarItem
-                            date="4 & 5 November"
-                            event="Italiaans etentje"
-                            until={new Date(2023, 10, 6)}
-                        />
-                        <CalendarItem
-                            date="24 December"
-                            event="Jeneveravond"
-                            until={new Date(2023, 11, 24)}
-                        />
-                        <CalendarItem
-                            date="5 & 6 April"
-                            event="White, Little & Grey (K)night"
-                            until={new Date(2024, 3, 7)}
-                        />
-                        <CalendarItem
-                            date="3-13 Augustus"
-                            event="Kamp"
-                            until={new Date(2024, 7, 14)}
-                        />
+                        {(this.state.calendarItems &&
+                            ((this.state.calendarItems.length == 0 && (
+                                <p>
+                                    Er is momenteel niets gepland, kom later nog
+                                    eens terug.
+                                </p>
+                            )) ||
+                                this.state.calendarItems.map((item) => (
+                                    <CalendarItem
+                                        key={item.event}
+                                        date={item.date}
+                                        event={item.event}
+                                        until={new Date(item.until)}
+                                    />
+                                )))) || <p>laden...</p>}
                         <br />
                         <WidgetSlideshow />
                         <SideWidget />
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -3,22 +3,34 @@ import React from "react";
 import "../styles/NewsItem.css";
 
 function getImg(props) {
-	if (props.imageUrl && props.imageUrl !== " ") return (<img src={props.imageUrl} alt="NewsImage" className="newsImg" />)
-	else return (<br/>)
+    if (props.imageUrl && props.imageUrl !== " ")
+        return <img src={props.imageUrl} alt="NewsImage" className="newsImg" />;
+    else return <br />;
 }
 
-const NewsItem = props => {
-  let img = ""
-	if (props.imageLink) img = (<a href={props.imageLink}>{getImg(props)}</a>)
-	else img = getImg(props)
+const NewsItem = (props) => {
+    if (props.until && props.until < new Date()) return null;
+    let img = "";
+    if (props.imageLink) img = <a href={props.imageLink}>{getImg(props)}</a>;
+    else img = getImg(props);
 
-  return (
-    <div className="newsBox">
-      <h2 className="titleNews">{props.title}</h2>
-      <div dangerouslySetInnerHTML={{ __html: props.text }} />
-			{img}
-    </div>
-  );
+    return (
+        <div className="newsBox">
+            <h2 className="titleNews">{props.title}</h2>
+
+            <p
+                style={
+                    ({ padding_left: 10 + "px" },
+                    { padding_right: 20 + "px" },
+                    { padding_bottom: 20 + "px" },
+                    { padding_top: 20 + "px" },
+                    { font_size: 1.17 + "em" })
+                }
+                dangerouslySetInnerHTML={{ __html: props.text }}
+            />
+            {img}
+        </div>
+    );
 };
 
 export default NewsItem;
